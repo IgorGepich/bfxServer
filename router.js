6cd999c609715d17a94541f13741a480c4d91d74
Sutulov <is.gepich@gmail.com>
2021-10-25 19:28:27 +0300

    1

--- router.js
+++ router.js
@@ -5,28 +5,56 @@ require('dotenv').config() // configuring file .env
 const app = express()
 const PORT = process.env.PORT
 
-const mtaRoutes = [process.env.MTADEV, process.env.MTAREAL, process.env.RASPILOCAL]
+const mtaRoutes = [process.env.MTADEVSI, process.env.MTAREAL, process.env.RASPILOCAL]
+
+// const log4js = require("log4js");
+// const log = require('./loggingConf');
+
+// log4js.configure({
+//         appenders: {
+//             out: { type: 'console' },
+//             task: { type: 'dateFile', filename: 'logs/task',"pattern":"-dd.log", alwaysIncludePattern:true },
+//             result: { type: 'dateFile', filename: 'logs/result',"pattern":"-dd.log", alwaysIncludePattern:true},
+//             error: { type: 'dateFile', filename: 'logs/error', "pattern":"-dd.log",alwaysIncludePattern:true},
+//             default: { type: 'dateFile', filename: 'logs/default', "pattern":"-dd.log",alwaysIncludePattern:true},
+//             rate: { type: 'dateFile', filename: 'logs/rate', "pattern":"-dd.log",alwaysIncludePattern:true}
+//         },
+//         categories: {
+//             default: { appenders: ['out','default'], level: 'info' },
+//             task: { appenders: ['task'], level: 'info'},
+//             result: { appenders: ['result'], level: 'info' },
+//             error: { appenders: ['error'], level: 'error' },
+//             rate: { appenders: ['rate'], level: 'info' }
+//         }
+//     }
+// );
+
+// let logger = log.getLogger('default')
+
 
 app.post('/', (req, res) => {
 
     let chunks = []
     let url = req.headers.host
-    console.log(url)
+    // console.log(url)
     let fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl
-    console.log(fullUrl)
+    // console.log(fullUrl)
+    // logger.result(fullUrl)
     const parts = fullUrl.split('/')
-    console.log(parts)
+    // console.log(parts)
     let targetUrl = parts[2]
-    console.log(targetUrl)
+    // console.log(targetUrl)
 
     req.on('data', function(data) {
         chunks.push(data)
 
     })
         .on('end', function() {
-        const data   = Buffer.concat(chunks)
+        const data = Buffer.concat(chunks)
         const reqBody = JSON.parse(data)
         console.log(reqBody)
+            for (let i in chunks) {console.log(' ', chunks[i])}
+
             switch (targetUrl){
                 case 'localhost:3000':
                     resendPostMethod(reqBody);
